46484f94ac96bb85928c2f97d888867c0984783c
Christine Morrill <christinenicolemorrill@gmail.com>
2023-05-22 21:20:43 -0400

    Iteration 2

--- ToDoList/ContentView.swift
+++ ToDoList/ContentView.swift
@@ -8,9 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
-    
-    @State var toDoItems: [ToDoItem] = []
+    @Environment(\.managedObjectContext) var context
    
+    private func deleteTask(offsets: IndexSet) {
+            withAnimation {
+                offsets.map { toDoItems[$0] }.forEach(context.delete)
+
+                do {
+                    try context.save()
+                } catch {
+                    print(error)
+                }
+            }
+        }
+    @FetchRequest(
+        entity: ToDo.entity(), sortDescriptors: [ NSSortDescriptor(keyPath: \ToDo.id, ascending: false) ])
+    
+    var toDoItems: FetchedResults<ToDo>
+    
     @State private var showNewTask = false
     
     var body: some View {
@@ -39,24 +54,24 @@ struct ContentView: View {
             List {
                 ForEach (toDoItems) { toDoItem in
                     if toDoItem.isImportant == true {
-                        Text("‼️" + toDoItem.title)
+                        Text("‼️" + (toDoItem.title ?? "No title"))
                     } else {
-                        Text(toDoItem.title)
+                        Text(toDoItem.title ?? "No title")
                     }
                 }
+                .onDelete(perform: deleteTask)
             }
             .listStyle(.plain)
         }
         if showNewTask {
-            NewToDoView(toDoItems: $toDoItems,showNewTask: $showNewTask, title: "", isImportant: false)
+            NewToDoView(showNewTask: $showNewTask, title: "", isImportant: false)
             
         }
-        
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
